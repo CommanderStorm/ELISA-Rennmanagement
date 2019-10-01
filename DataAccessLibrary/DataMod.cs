437fe0de5afee048888f8c6d8769b1ac474f2d09
Frank <frank@elsinga.de>
2019-10-01 20:12:47 +0200

    Fixed Using-statements

--- DataAccessLibrary/DataMod.cs
+++ DataAccessLibrary/DataMod.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
